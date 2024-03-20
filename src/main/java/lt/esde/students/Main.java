e23e83ac856bf96c7d458a2fae0df2628c484efb
Mihail Komarov <michaelkomarov03@gmail.com>
2024-03-20 13:45:28 +0200

    feature01: working snippet based on WriteExifMetadataExample.java

--- src/main/java/lt/esde/students/Main.java
+++ src/main/java/lt/esde/students/Main.java
@@ -1,43 +1,25 @@
 package lt.esde.students;
 
-
-import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.ImagingException;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.FileTime;
-import java.text.ParseException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-
-import static lt.esde.students.ExifUtil.*;
-
-import lt.esde.students.ExifUtil.*;
-
-import static lt.esde.students.FileUtil.*;
 
 public class Main {
     public static final String TEST_IMG_FOLDER_PATH = Paths.get("")
-            .toAbsolutePath().toString() + File.separator + "testimg";
+            .toAbsolutePath() + File.separator + "testimg";
     public static final String TEST_IMG_WITH_METADATA_PATH = Paths.get("")
-            .toAbsolutePath().toString() + File.separator + "testimg" + File.separator + "eifel.jpg";
+            .toAbsolutePath() + File.separator + "testimg" + File.separator + "eifel.jpg";
     public static final String TEST_IMG_WITHOUT_METADATA_PATH = Paths.get("")
-            .toAbsolutePath().toString() + File.separator + "testimg" + File.separator + "maricat.jpg";
+            .toAbsolutePath() + File.separator + "testimg" + File.separator + "maricat.jpg";
 
-    public static void main(String[] args) throws IOException, ImageReadException, ParseException {
+    public static void main(String[] args) throws ImagingException, IOException {
 
 //        File inputImage = new File("D:\\Test.tiff"); // Good
 //        File inputImage = new File("D:\\Test.jpg"); // Good
 //        File inputImage = new File("D:\\Test.png"); // Exception: png has no exif support :(
-        //writeExifTag(inputImage, "D:\\Test.tiff", 0x9003);
-
-
-        //getCreationDateTime(TEST_IMG_WITHOUT_METADATA_PATH);
-        //convertToTiff();
-
+        WriteExifMetadataExample example = new WriteExifMetadataExample();
+        example.setExifGPSTag(new File(TEST_IMG_WITHOUT_METADATA_PATH), new File(TEST_IMG_FOLDER_PATH + File.separator + "test1.jpg"));
     }
 }
