ef8ed9978dd56e4a9455cbfc70b4b8eff302c3c4
Mihail Komarov <michaelkomarov03@gmail.com>
2024-03-21 10:37:03 +0200

    feature01: remove updateHourOnFile()

--- src/main/java/lt/esde/students/ExifUtil.java
+++ src/main/java/lt/esde/students/ExifUtil.java
@@ -6,28 +6,20 @@ import org.apache.commons.imaging.Imaging;
 import org.apache.commons.imaging.common.ImageMetadata;
 import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata;
 import org.apache.commons.imaging.formats.jpeg.exif.ExifRewriter;
-import org.apache.commons.imaging.formats.tiff.TiffField;
 import org.apache.commons.imaging.formats.tiff.TiffImageMetadata;
 import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
-import org.apache.commons.imaging.formats.tiff.constants.TiffTagConstants;
 import org.apache.commons.imaging.formats.tiff.fieldtypes.FieldType;
-import org.apache.commons.imaging.formats.tiff.taginfos.TagInfo;
 import org.apache.commons.imaging.formats.tiff.write.TiffOutputDirectory;
 import org.apache.commons.imaging.formats.tiff.write.TiffOutputField;
 import org.apache.commons.imaging.formats.tiff.write.TiffOutputSet;
 
 import java.io.*;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Objects;
 
 import static lt.esde.students.FileUtil.getCreationDateTime;
-import static lt.esde.students.Main.TEST_IMG_FOLDER_PATH;
-import static lt.esde.students.Main.TEST_IMG_WITH_METADATA_PATH;
 
 public class ExifUtil {
 
@@ -98,12 +90,10 @@ public class ExifUtil {
             }
             // add field
             try {
-
                 LocalDateTime dateTime = getCreationDateTime(inputImage.getAbsolutePath());
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
                 String dateTimeString = dateTime.format(formatter);
 
-//                    String fieldData = "Hallo";
                 TiffOutputField imageDateTimeOriginal = new TiffOutputField(
                         ExifTagConstants.EXIF_TAG_DATE_TIME_ORIGINAL,
                         FieldType.ASCII,
@@ -123,59 +113,5 @@ public class ExifUtil {
         }
 
         return returnValue;
-    }
-
-
-    public static void updateHourOnFile(String inputFileName, String outputFileName, int hourChange) throws ImageReadException, IOException, ParseException, ImageWriteException {
-        File inputFile = new File(inputFileName);
-        final ImageMetadata metadata = getMetadata(inputFile);
-
-        if (metadata != null && metadata instanceof JpegImageMetadata) {
-            final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
-            System.out.println("we have metadata");
-            final TiffField dateTimeField = jpegMetadata.findEXIFValueWithExactMatch(TiffTagConstants.TIFF_TAG_DATE_TIME);
-            SimpleDateFormat SDF = new SimpleDateFormat("yyyy:MM:dd kk:mm:ss");
-            Date pictureDate = SDF.parse((String) dateTimeField.getValue());
-            System.out.println("date: " + pictureDate);
-            Calendar cal = Calendar.getInstance();
-            cal.clear();
-            cal.setTime(pictureDate);
-            cal.add(Calendar.HOUR, hourChange);
-            System.out.println("new date time: " + cal.getTime());
-
-            TiffOutputSet outputSet = null;
-            try (FileOutputStream fos = new FileOutputStream(outputFileName); OutputStream os = new BufferedOutputStream(fos)) {
-                final TiffImageMetadata exif = jpegMetadata.getExif();
-                outputSet = exif.getOutputSet();
-                if (outputSet != null) {
-                    final TiffOutputDirectory exifDirectory = outputSet.getOrCreateExifDirectory();
-                    FieldType originalFieldType = null;
-                    TagInfo originalTagInfo = null;
-                    TiffOutputField dateTimeFieldOriginal = exifDirectory.findField(DATE_TIME_ORIGINAL); // DateTimeOriginal
-                    if (dateTimeFieldOriginal != null) {
-                        originalFieldType = dateTimeFieldOriginal.fieldType;
-                        originalTagInfo = dateTimeFieldOriginal.tagInfo;
-                    }
-
-                    exifDirectory.removeField(DATE_TIME_ORIGINAL);
-                    String updatedDateString = SDF.format(cal.getTime());
-                    final TiffOutputField dateTimeOutputField = new TiffOutputField(originalTagInfo, originalFieldType, updatedDateString.length(), updatedDateString.getBytes());
-                    exifDirectory.add(dateTimeOutputField);
-                    new ExifRewriter().updateExifMetadataLossless(inputFile, os, outputSet);
-                } else {
-                    System.out.println("no cigar on output set from file " + inputFileName);
-                }
-            }
-        } else {
-            System.out.println("no cigar on file " + inputFileName);
-        }
-
-    }
-
-    public static void main(String[] args) throws ImageWriteException, ImageReadException, IOException, ParseException {
-        String inputFileName = TEST_IMG_WITH_METADATA_PATH;
-        String outputFileName = TEST_IMG_FOLDER_PATH + "output.tiff";
-        int hoursChange = -500;
-        updateHourOnFile(inputFileName, outputFileName, hoursChange);
     }
 }
