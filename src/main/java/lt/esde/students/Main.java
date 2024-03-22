15ff7bc2abe3ffd4bee60ffa2a7ca8147be85b07
Mihail Komarov <michaelkomarov03@gmail.com>
2024-03-22 20:42:31 +0200

    feature06: parser in progress

--- src/main/java/lt/esde/students/Main.java
+++ src/main/java/lt/esde/students/Main.java
@@ -1,7 +1,11 @@
 package lt.esde.students;
 
+import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
+
 import java.io.File;
 import java.nio.file.Paths;
+
+import static lt.esde.students.ExifUtil.readExifTag;
 
 public class Main {
     /**
@@ -22,7 +26,33 @@ public class Main {
     public static final String TEST_IMG_WITHOUT_METADATA_PATH = Paths.get("")
             .toAbsolutePath() + File.separator + "testimg" + File.separator + "maricat.jpg";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        File paramFile = new File(TEST_IMG_WITH_METADATA_PATH);
+
+        //parseDateTimeOriginal()
+
+        String dateTimeOriginal = readExifTag(paramFile, ExifTagConstants.EXIF_TAG_DATE_TIME_ORIGINAL);
+
+        //parseOffsetTimeOriginal()
+
+        //String offsetTimeOriginal = readExifTag(paramFile, )
+
+        //parseModifyDate()
+        //parseOffsetTime()
+
+        // TODO: is this the right constant?
+        //String offsetTime = readExifTag(paramFile, ExifTagConstants.EXIF_TAG_EXIF_OFFSET);
+
+        //parseExifImageWidth()
+        //parseExifImageHeight()
+        //parseOriginalDefaultFinalSize()
+        //parseOriginalBestQualitySize()
+        //parseXPAuthor()
+        //parseSoftware()
+        //parseImageHistory()
+        //parseImageDescription()
+
+        System.out.println(0x9010);
 
     }
 }
