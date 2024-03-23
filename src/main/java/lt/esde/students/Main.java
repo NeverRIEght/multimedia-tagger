b6909f9286fee2d3a34e9c954972c32dbc8489d2
Mihail Komarov <michaelkomarov03@gmail.com>
2024-03-23 16:13:21 +0200

    feature06: testing

--- src/main/java/lt/esde/students/Main.java
+++ src/main/java/lt/esde/students/Main.java
@@ -4,6 +4,9 @@ import com.drew.imaging.ImageMetadataReader;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.file.FileSystemDirectory;
+import com.drew.metadata.file.FileSystemMetadataReader;
 import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
 import org.apache.commons.imaging.formats.tiff.constants.MicrosoftTagConstants;
 import org.apache.commons.imaging.formats.tiff.constants.TiffEpTagConstants;
@@ -11,7 +14,7 @@ import org.apache.commons.imaging.formats.tiff.constants.TiffTagConstants;
 
 import java.io.File;
 import java.nio.file.Paths;
-import java.util.HashMap;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static lt.esde.students.ExifUtil.*;
@@ -37,34 +40,38 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-//        HashMap<String, String> tagsMap1 = parseFileMetata(new File(TEST_IMG_WITH_METADATA_PATH));
-        HashMap<String, String> tagsMap2 = readExifTags(new File(TEST_IMG_WITH_METADATA_PATH));
+        HashMap<String, String> tagsMap1 = parseFileMetata(new File(TEST_IMG_WITH_METADATA_PATH));
 
         Metadata metadata = ImageMetadataReader.readMetadata(new File(TEST_IMG_WITH_METADATA_PATH));
 
-
-
-
-//        String content1 = tagsMap1.entrySet()
-//                .stream()
-//                .map(e -> e.getKey() + "=\"" + e.getValue() + "\"")
-//                .collect(Collectors.joining("\n"));
-
-        String content2 = tagsMap2.entrySet()
+        String content1 = tagsMap1.entrySet()
                 .stream()
                 .map(e -> e.getKey() + "=\"" + e.getValue() + "\"")
                 .collect(Collectors.joining("\n"));
 
+        System.out.println(content1);
+
+        System.out.println("--------");
 
 
-        for (Directory directory : metadata.getDirectories()) {
-            for (Tag tag : directory.getTags()) {
-                System.out.println(tag);
+
+        // obtain the Exif directory
+        ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(FileSystemDirectory.class);
+        com.drew.metadata.file.FileSystemMetadataReader reader = new FileSystemMetadataReader();
+
+
+        // query the tag's value
+        Date date = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+
+        Iterable<Directory> directories = metadata.getDirectories();
+        Iterator<Directory> iterator = directories.iterator();
+        while(iterator.hasNext()) {
+            Directory dir = iterator.next();
+            Collection<Tag> tags = dir.getTags();
+            for(Tag tag: tags) {
+                System.out.println(tag.getTagName() + " - " + tag.getDescription() + " - " + tag.getTagTypeHex());
             }
         }
-
-        System.out.println("\n\n\n");
-        System.out.println(content2);
 
     }
 }
