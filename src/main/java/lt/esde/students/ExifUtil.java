12cb8c66a2f7092779fe35cebaff2dc7205a7bad
Mihail Komarov <michaelkomarov03@gmail.com>
2024-03-31 13:27:07 +0300

    feature20: move readExifTag() and readExifTags() to ExifReader

--- src/main/java/lt/esde/students/ExifUtil.java
+++ src/main/java/lt/esde/students/ExifUtil.java
@@ -4,16 +4,10 @@ import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.file.FileSystemDirectory;
-import org.apache.commons.imaging.ImageReadException;
-import org.apache.commons.imaging.Imaging;
-import org.apache.commons.imaging.common.ImageMetadata;
-import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata;
-import org.apache.commons.imaging.formats.tiff.TiffField;
 import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
 import org.apache.commons.imaging.formats.tiff.constants.MicrosoftTagConstants;
 import org.apache.commons.imaging.formats.tiff.constants.TiffEpTagConstants;
 import org.apache.commons.imaging.formats.tiff.constants.TiffTagConstants;
-import org.apache.commons.imaging.formats.tiff.taginfos.TagInfo;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,66 +15,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 
 import static lt.esde.students.Main.TEST_IMG_WITH_METADATA_PATH;
 
 public class ExifUtil {
-    /**
-     * Parses the specific EXIF tag to <code>String</code>
-     * <p>
-     *
-     * @param fromFile file to parse
-     * @param tagInfo  <code>TagInfo</code> of the EXIF tag to read
-     * @return <code>String</code> with the data found. Might be null in case the field is empty
-     * @see ExifTagConstants
-     */
-    public static String readExifTag(final File fromFile, final TagInfo tagInfo) {
-        try {
-            final ImageMetadata metadata = Imaging.getMetadata(fromFile);
-            if (metadata instanceof JpegImageMetadata) {
-                final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
-
-                final TiffField field = jpegMetadata.findEXIFValueWithExactMatch(tagInfo);
-                if (field != null) {
-                    return field.getValueDescription();
-                }
-            }
-        } catch (ImageReadException | IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return null;
-    }
-
-    /**
-     * Parses the whole set of EXIF tags for specific file and returns it as a <code>Hashmap</code>
-     * <p>
-     *
-     * @param fromFile file to parse
-     * @return <code>Hashmap</code> of the non-null fields set. Might be null if the set is empty
-     */
-    public static HashMap<String, String> readExifTags(final File fromFile) {
-        HashMap<String, String> tagsMap = new HashMap<>();
-
-        try {
-            final ImageMetadata metadata = Imaging.getMetadata(fromFile);
-            if (metadata instanceof JpegImageMetadata) {
-                final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
-                final List<ImageMetadata.ImageMetadataItem> items = jpegMetadata.getItems();
-
-                for (final ImageMetadata.ImageMetadataItem item : items) {
-                    String tagString = item.toString();
-                    tagsMap.put(tagString.substring(0, tagString.indexOf(":")), tagString.substring(tagString.indexOf(":") + 2));
-                }
-            }
-        } catch (ImageReadException | IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return tagsMap;
-    }
-
     public static HashMap<String, String> parseFileMetadata(final File fromFile) throws ImageProcessingException, IOException {
 
 
---
+++ src/main/java/lt/esde/students/metadata/ExifReader.java
@@ -0,0 +1,71 @@
+package lt.esde.students.metadata;
+
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.common.ImageMetadata;
+import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata;
+import org.apache.commons.imaging.formats.tiff.TiffField;
+import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
+import org.apache.commons.imaging.formats.tiff.taginfos.TagInfo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExifReader {
+    /**
+     * Parses the specific EXIF tag to <code>String</code>
+     * <p>
+     *
+     * @param fromFile file to parse
+     * @param tagInfo  <code>TagInfo</code> of the EXIF tag to read
+     * @return <code>String</code> with the data found. Might be null in case the field is empty
+     * @see ExifTagConstants
+     */
+    public static String readExifTag(final File fromFile, final TagInfo tagInfo) {
+        try {
+            final ImageMetadata metadata = Imaging.getMetadata(fromFile);
+            if (metadata instanceof JpegImageMetadata) {
+                final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
+
+                final TiffField field = jpegMetadata.findEXIFValueWithExactMatch(tagInfo);
+                if (field != null) {
+                    return field.getValueDescription();
+                }
+            }
+        } catch (ImageReadException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
+
+    /**
+     * Parses the whole set of EXIF tags for specific file and returns it as a <code>Hashmap</code>
+     * <p>
+     *
+     * @param fromFile file to parse
+     * @return <code>Hashmap</code> of the non-null fields set. Might be null if the set is empty
+     */
+    public static HashMap<String, String> readExifTags(final File fromFile) {
+        HashMap<String, String> tagsMap = new HashMap<>();
+
+        try {
+            final ImageMetadata metadata = Imaging.getMetadata(fromFile);
+            if (metadata instanceof JpegImageMetadata) {
+                final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
+                final List<ImageMetadata.ImageMetadataItem> items = jpegMetadata.getItems();
+
+                for (final ImageMetadata.ImageMetadataItem item : items) {
+                    String tagString = item.toString();
+                    tagsMap.put(tagString.substring(0, tagString.indexOf(":")), tagString.substring(tagString.indexOf(":") + 2));
+                }
+            }
+        } catch (ImageReadException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return tagsMap;
+    }
+}
