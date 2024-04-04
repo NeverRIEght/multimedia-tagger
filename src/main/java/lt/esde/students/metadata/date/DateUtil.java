47eda2f8472b8997007a4e09e5b053512b78fde1
Mihail Komarov <michaelkomarov03@gmail.com>
2024-04-04 12:29:06 +0300

    feature17: refactor and optimise for getAllMetadataDates()

--- src/main/java/lt/esde/students/metadata/date/DateUtil.java
+++ src/main/java/lt/esde/students/metadata/date/DateUtil.java
@@ -11,18 +11,27 @@ import static lt.esde.students.metadata.exif.ExifReader.readExifTags;
 
 public class DateUtil {
     public static List<LocalDateTime> getAllMetadataDates(File fromFile) {
+        if (!fromFile.isFile()) {
+            throw new NullPointerException("File does not exist");
+        }
+
         Locale.setDefault(Locale.ENGLISH);
 
         HashMap<String, String> map = readExifTags(fromFile);
         List<String> dateStrings = new ArrayList<>();
 
+        Pattern timePattern = Pattern.compile("[0-9]{2}\\u003A[0-9]{2}\\u003A[0-9]{2}");
+        Pattern dateKeyPattern = Pattern.compile("date|Date");
+
+        String datePatternString1 = "([0-9]{4}\\u003A[0-9]{2}\\u003A[0-9]{2})";
+        String datePatternString2 = "([0-9]{4}\\u002D[0-9]{2}\\u002D[0-9]{2})";
+        Pattern datePattern = Pattern.compile(datePatternString1 + "|" + datePatternString2);
+
         for (Map.Entry<String, String> item : map.entrySet()) {
             String itemValueStr = item.getValue();
             String itemKeyStr = item.getKey();
 
-            Pattern dateValuePattern = Pattern.compile("[0-9]{2}\\u003A[0-9]{2}\\u003A[0-9]{2}");
-            Matcher dateValueMatcher = dateValuePattern.matcher(itemValueStr);
-            Pattern dateKeyPattern = Pattern.compile("date|Date");
+            Matcher dateValueMatcher = timePattern.matcher(itemValueStr);
             Matcher dateKeyMatcher = dateKeyPattern.matcher(itemKeyStr);
 
             if (dateValueMatcher.find() || dateKeyMatcher.find()) {
@@ -30,60 +39,41 @@ public class DateUtil {
             }
         }
 
-        System.out.println(dateStrings.toString().replace(", ", "\n"));
+        List<LocalDateTime> outputDates = new ArrayList<>();
 
-
-        List<LocalDateTime> dates = new ArrayList<>();
-
-        Pattern timePattern = Pattern.compile("[0-9]{2}\\u003A[0-9]{2}\\u003A[0-9]{2}");
-        String datePatternString1 = "([0-9]{4}\\u003A[0-9]{2}\\u003A[0-9]{2})";
-        String datePatternString2 = "([0-9]{4}\\u002D[0-9]{2}\\u002D[0-9]{2})";
-        Pattern datePattern = Pattern.compile(datePatternString1 + "|" + datePatternString2);
-
-        for (int i = 0; i < dateStrings.size(); i++) {
-            String currentString = dateStrings.get(i);
-
-            Matcher dateMatcher = datePattern.matcher(currentString);
+        for (String currentDateString : dateStrings) {
+            Matcher dateMatcher = datePattern.matcher(currentDateString);
             String dateString = "";
             if (dateMatcher.find()) {
                 int startIndex = dateMatcher.start();
                 int endIndex = dateMatcher.end();
-                dateString = currentString.substring(startIndex, endIndex);
+                dateString = currentDateString.substring(startIndex, endIndex);
                 dateString = dateString.replaceAll(":", "-");
-                currentString = currentString.substring(endIndex);
+                currentDateString = currentDateString.substring(endIndex);
             }
 
-            Matcher timeMatcher = timePattern.matcher(currentString);
-            String timeString = "";
-            if (timeMatcher.find()) {
-                int startIndex = timeMatcher.start();
-                int endIndex = timeMatcher.end();
-                timeString = currentString.substring(startIndex, endIndex);
-            }
-
-            //if() Fri Mar 29 20:35:59 +02:00 2024 parse
             if (dateString.isEmpty()) {
                 Pattern monthDayPattern = Pattern.compile(
                         "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\s[0-9]{2}"
                 );
                 Pattern yearPattern = Pattern.compile("\\u003A[0-9]{2}\\s[0-9]{4}");
 
-                Matcher monthDayMatcher = monthDayPattern.matcher(currentString);
+                Matcher monthDayMatcher = monthDayPattern.matcher(currentDateString);
                 String monthString = "";
                 String dayString = "";
                 if (monthDayMatcher.find()) {
                     int startIndex = monthDayMatcher.start();
                     int endIndex = monthDayMatcher.end();
-                    monthString = currentString.substring(startIndex, endIndex - 3);
-                    dayString = currentString.substring(startIndex + 4, endIndex);
+                    monthString = currentDateString.substring(startIndex, endIndex - 3);
+                    dayString = currentDateString.substring(startIndex + 4, endIndex);
                 }
 
-                Matcher yearMatcher = yearPattern.matcher(currentString);
+                Matcher yearMatcher = yearPattern.matcher(currentDateString);
                 String yearString = "";
                 if (yearMatcher.find()) {
                     int startIndex = yearMatcher.start();
                     int endIndex = yearMatcher.end();
-                    yearString = currentString.substring(startIndex + 4, endIndex);
+                    yearString = currentDateString.substring(startIndex + 4, endIndex);
                 }
 
                 if (!monthString.isEmpty() && !dayString.isEmpty() && !yearString.isEmpty()) {
@@ -106,15 +96,23 @@ public class DateUtil {
                 }
             }
 
+            Matcher timeMatcher = timePattern.matcher(currentDateString);
+            String timeString = "";
+            if (timeMatcher.find()) {
+                int startIndex = timeMatcher.start();
+                int endIndex = timeMatcher.end();
+                timeString = currentDateString.substring(startIndex, endIndex);
+            }
+
             if (!timeString.isEmpty() && !dateString.isEmpty()) {
                 LocalDateTime dateTime = LocalDateTime.parse(dateString + "T" + timeString);
-                dates.add(dateTime);
+                outputDates.add(dateTime);
             }
         }
 
-        dates = dates.stream().distinct().collect(Collectors.toList());
+        outputDates = outputDates.stream().distinct().collect(Collectors.toList());
 
-        return dates;
+        return outputDates;
     }
 
     public static LocalDateTime getOldestDate(List<LocalDateTime> dates) {
