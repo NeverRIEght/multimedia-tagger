da70986cfeb5cb1c5cfab65dd86d19e29af79685
Mihail Komarov <michaelkomarov03@gmail.com>
2024-04-26 19:09:33 +0300

    feature/35: add constant for the testimage folder

--- src/main/java/lt/esde/students/Main.java
+++ src/main/java/lt/esde/students/Main.java
@@ -5,22 +5,26 @@ import java.nio.file.Paths;
 
 public class Main {
     /**
+     * Name of the folder with test images
+     */
+    public static final String TEST_FOLDER_NAME = "testimg";
+    /**
      * Path to the folder with test images
      */
     public static final String TEST_IMG_FOLDER_PATH = Paths.get("")
-            .toAbsolutePath() + File.separator + "testimg";
+            .toAbsolutePath() + File.separator + TEST_FOLDER_NAME;
 
     /**
      * Path to the test jpg file, which has exif field with creation date
      */
     public static final String TEST_IMG_WITH_METADATA_PATH = Paths.get("")
-            .toAbsolutePath() + File.separator + "testimg" + File.separator + "eifel.jpg";
+            .toAbsolutePath() + File.separator + TEST_FOLDER_NAME + File.separator + "eifel.jpg";
 
     /**
      * Path to the test jpg file, which has no exif field with creation date
      */
     public static final String TEST_IMG_WITHOUT_METADATA_PATH = Paths.get("")
-            .toAbsolutePath() + File.separator + "testimg" + File.separator + "maricat.jpg";
+            .toAbsolutePath() + File.separator + TEST_FOLDER_NAME + File.separator + "maricat.jpg";
 
     public static void main(String[] args) {
 
