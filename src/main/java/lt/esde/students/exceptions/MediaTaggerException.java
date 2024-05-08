dff377435baf248fd9057f545fe7ce9eca42d18f
execute <3xecvte@gmail.com>
2024-05-08 13:57:16 +0300

    feature/32: add documentation for main exception

--- src/main/java/lt/esde/students/exceptions/MediaTaggerException.java
+++ src/main/java/lt/esde/students/exceptions/MediaTaggerException.java
@@ -1,13 +1,27 @@
 package lt.esde.students.exceptions;
 
+/**
+ * Main exception for hierarchy custom exceptions
+ */
 public class MediaTaggerException extends RuntimeException{
 
+    /**
+     * Message for exception
+     */
     private final String message;
 
+    /**
+     * Constructor
+     * @param message
+     */
     public MediaTaggerException(String message) {
         this.message = message;
     }
 
+    /**
+     * Get message from exception
+     * @return Custom message
+     */
     @Override
     public String getMessage() {
         StringBuilder sb = new StringBuilder();
