ab411f533efb6a96f19c68b30d9228dcd2542926
execute <3xecvte@gmail.com>
2024-05-08 14:21:45 +0300

    feature/32: add documentation for double usage files

--- src/main/java/lt/esde/students/exceptions/file/DoubleFileUsageException.java
+++ src/main/java/lt/esde/students/exceptions/file/DoubleFileUsageException.java
@@ -4,10 +4,26 @@ import lt.esde.students.exceptions.MediaTaggerException;
 
 import java.io.File;
 
+/**
+ * Exception root for exceptions which connected to usage of two files
+ */
 public class DoubleFileUsageException extends MediaTaggerException {
+    /**
+     * First file which has problems
+     */
     private final File firstFile;
+
+    /**
+     * Second file which has problems
+     */
     private final File secondFile;
 
+    /**
+     * Constructor for exception
+     * @param message Message for exception
+     * @param firstFile First file which has problems
+     * @param secondFile Second file which has problems
+     */
     public DoubleFileUsageException(String message, File firstFile, File secondFile) {
         super(message);
 
@@ -15,10 +31,18 @@ public class DoubleFileUsageException extends MediaTaggerException {
         this.secondFile = secondFile;
     }
 
+    /**
+     * Get first file
+     * @return First file with which has problems
+     */
     public File getFirstFile() {
         return this.firstFile;
     }
 
+    /**
+     * Get second file
+     * @return Second file with which has problems
+     */
     public File getSecondFile() {
         return this.secondFile;
     }
