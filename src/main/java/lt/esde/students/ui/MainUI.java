06f1944f1ad2eea0b138c6df15fe9b3b0426b96c
Mihail Komarov <michaelkomarov03@gmail.com>
2024-06-26 20:51:46 +0300

    feature/43: remove unnecessary imports in MainUI

--- src/main/java/lt/esde/students/ui/MainUI.java
+++ src/main/java/lt/esde/students/ui/MainUI.java
@@ -4,24 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import lt.esde.students.Main;
 
-import java.io.File;
 import java.io.IOException;
 
-import static lt.esde.students.Main.SOURCE_CODE_PATH;
-
 public class MainUI extends Application {
-
-    public static final String PATH_TO_FIRST_SCENE = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "main-page.fxml";
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(new File("D:\\Programming\\Java\\multimedia-tagger\\src\\main\\resources\\main-page.fxml").toURI().toURL());
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getClassLoader().getResource("main-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 600);
 
