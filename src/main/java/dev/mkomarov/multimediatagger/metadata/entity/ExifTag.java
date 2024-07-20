4cc340bf0129e143c09f8f6761227c0bdba2dbe9
Mihail Komarov <michaelkomarov03@gmail.com>
2024-07-20 20:32:27 +0300

    feature/50: Convert ExifTag to record

--- src/main/java/dev/mkomarov/multimediatagger/metadata/entity/ExifTag.java
+++ src/main/java/dev/mkomarov/multimediatagger/metadata/entity/ExifTag.java
@@ -1,36 +1,6 @@
 package dev.mkomarov.multimediatagger.metadata.entity;
 
-import java.util.Objects;
-
-public class ExifTag {
-    private final String name;
-    private final String value;
-
-    public ExifTag(String name, String value) {
-        this.name = name;
-        this.value = value;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ExifTag exifTag = (ExifTag) o;
-        return Objects.equals(name, exifTag.name) && Objects.equals(value, exifTag.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, value);
-    }
+public record ExifTag(String name, String value) {
 
     @Override
     public String toString() {
