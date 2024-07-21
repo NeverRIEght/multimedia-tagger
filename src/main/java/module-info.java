0611b5f5d46570042c7ef3cdd1693425caa15017
Mihail Komarov <michaelkomarov03@gmail.com>
2024-07-21 19:08:06 +0300

    feature/49: update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module lt.esde.students {
     requires org.apache.commons.imaging;
     requires metadata.extractor;
     requires org.json;
+    requires org.apache.logging.log4j;
 
 
     exports dev.mkomarov.multimediatagger.ui to javafx.graphics;
