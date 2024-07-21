d7dead0245c0b25f3e575ecea0ff1ff8081f1ce0
Mihail Komarov <michaelkomarov03@gmail.com>
2024-07-21 14:21:11 +0300

    feature/53: update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,7 @@ module lt.esde.students {
     requires org.controlsfx.controls;
     requires java.desktop;
     requires org.apache.commons.imaging;
-    requires org.apache.commons.io;
     requires metadata.extractor;
-    requires htmlunit;
     requires org.json;
 
 
